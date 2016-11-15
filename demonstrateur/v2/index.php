42dafd2234221e598c877187e25ad1d972138109
PF <pierre-francois.gimenez@irit.fr>
2016-11-15 10:35:12 +0100

    Description algo et dataset dans le demonstrateur

--- demonstrateur/v2/index.php
+++ demonstrateur/v2/index.php
@@ -20,6 +20,52 @@
         <?php
                 include("cnnx.php");
         ?>
+<table border="0" width="100%">
+            <tr>
+                <td><b>Alarm:</b> classic Bayesian network dataset</td>
+                <td><b>DRC:</b> experimental Bayesian inference</td>
+            </tr>
+            <tr>
+                <td><b>Champi:</b> dataset on mushrooms</td>
+                <td><b>RC:</b> Bayesian inference</td>
+            </tr>
+            <tr>
+                <td><b>Child:</b> classic Bayesian network dataset</td>
+                <td><b>Jointree:</b> fast Bayesian inference</td>
+            </tr>
+            <tr>
+                <td><b>Congress:</b> dataset on american congress</td>
+                <td><b>v-maj:</b> weighted majority voter (based on neighbourhood)</td>
+            </tr>
+            <tr>
+                <td><b>Hailfinder:</b> classic Bayesian network dataset</td>
+                <td><b>v-pop:</b> most popular choice (based on neighbourhood)</td>
+            </tr>
+            <tr>
+                <td><b>Insurance:</b> classic Bayesian network dataset</td>
+                <td><b>v-naif:</b> naïve Bayes voter (based on neighbourhood)</td>
+            </tr>
+            <tr>
+                <td><b>Renault_small_header:</b> Renault "small" dataset (without constraints)</td>
+                <td><b>naif:</b> naïve Bayesian network</td>
+            </tr>
+          <tr>
+                <td><b>Renault_medium_header:</b> Renault "medium" dataset (without constraints)</td>
+            </tr>
+            <tr>
+                <td><b>Renault_medium_header:</b> Renault "big" dataset (without constraints)</td>
+            </tr>
+<tr>
+                <td><b>Renault_small_header_contraintes:</b> Renault "small" dataset (with constraints)</td>
+            </tr>
+            <tr>
+                <td><b>Renault_medium_header_contraintes:</b> Renault "medium" dataset (with constraints)</td>
+            </tr>
+            <tr>
+                <td><b>Renault_medium_header_contraintes:</b> Renault "big" dataset (with constraints)</td>
+            </tr>
+
+        </table>
     </body>
 </html>
 <?php
