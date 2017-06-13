c6d3c578eb0bc5a7c5ba08cf1f663b28165d7b50
PF <pierre-francois.gimenez@irit.fr>
2017-06-13 20:13:27 +0200

    Experiment 4

--- experiments/exp4/run.sh
+++ experiments/exp4/run.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
-echo Experiment on recommendation precision with different BN-learning algorithms
+echo Experiment on recommendation precision with new dataset on old BN
+echo Trimester 1
+(cd ../.. ; ./run.sh Recommandation drc ../experiments/exp4/renault_small_header_appl_part1 | tee experiments/exp4/drc_trim1_small_result)
+(cd ../.. ; ./run.sh Recommandation jointree ../experiments/exp4/renault_small_header_appl_part1 | tee experiments/exp4/jointree_trim1_small_result)
+
+echo Trimester 2
+(cd ../.. ; ./run.sh Recommandation drc ../experiments/exp4/renault_small_header_appl_part2 | tee experiments/exp4/drc_trim2_small_result)
+(cd ../.. ; ./run.sh Recommandation jointree ../experiments/exp4/renault_small_header_appl_part2 | tee experiments/exp4/jointree_trim2_small_result)
+
+echo Trimester 3
+(cd ../.. ; ./run.sh Recommandation drc ../experiments/exp4/renault_small_header_appl_part3 | tee experiments/exp4/drc_trim3_small_result)
+(cd ../.. ; ./run.sh Recommandation jointree ../experiments/exp4/renault_small_header_appl_part3 | tee experiments/exp4/jointree_trim3_small_result)
+
+echo Trimester 4
+(cd ../.. ; ./run.sh Recommandation drc ../experiments/exp4/renault_small_header_appl_part4 | tee experiments/exp4/drc_trim4_small_result)
+(cd ../.. ; ./run.sh Recommandation jointree ../experiments/exp4/renault_small_header_appl_part4 | tee experiments/exp4/jointree_trim4_small_result)
+
+echo Experiment on recommendation precision with new dataset on old BN 
+
 echo Trimester 1
 (cd ../.. ; ./run.sh Recommandation drc ../experiments/exp4/renault_small_header_part1 | tee experiments/exp4/drc_trim1_small_result)
 (cd ../.. ; ./run.sh Recommandation jointree ../experiments/exp4/renault_small_header_part1 | tee experiments/exp4/jointree_trim1_small_result)
@@ -15,3 +33,4 @@ echo Trimester 3
 echo Trimester 4
 (cd ../.. ; ./run.sh Recommandation drc ../experiments/exp4/renault_small_header_part4 | tee experiments/exp4/drc_trim4_small_result)
 (cd ../.. ; ./run.sh Recommandation jointree ../experiments/exp4/renault_small_header_part4 | tee experiments/exp4/jointree_trim4_small_result)
+
