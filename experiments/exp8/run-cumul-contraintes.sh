6f1b526f13d355f83153fcebe9fece72e03be5b9
PF <pierre-francois.gimenez@irit.fr>
2017-09-11 10:24:21 +0200

    Experiment 8 files

---
+++ experiments/exp8/run-cumul-contraintes.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo Experiment on recommendation precision with new dataset on old BN 
+
+echo Trimester 2
+#(cd ../.. ; ./run.sh Recom2 drc experiments/exp8/renault_small_T2 10 -s 10 -e -rb experiments/exp8/ -x 1 experiments/exp8/renault_small_T1 -c experiments/exp8/contraintes.xml | tee experiments/exp8/drc_trim2_small_result_cumul_const)
+#(cd ../.. ; ./run.sh Recom2 jointree experiments/exp8/renault_small_T2 10 -s 10 -e -rb experiments/exp8/ -x 1 experiments/exp8/renault_small_T1 -c experiments/exp8/contraintes.xml | tee experiments/exp8/jointree_trim2_small_result_cumul_const)
+#(cd ../.. ; ./run.sh Recom2 oracle experiments/exp8/renault_small_T2 10 -s 10 -e -rb experiments/exp8/ -x 1 experiments/exp8/renault_small_T1 -c experiments/exp8/contraintes.xml | tee experiments/exp8/oracle_trim2_small_result_cumul_const)
+
+echo Trimester 3
+(cd ../.. ; ./run.sh Recom2 drc experiments/exp8/renault_small_T3 10 -s 10 -e -rb experiments/exp8/ -x 2 experiments/exp8/renault_small_T1/ experiments/exp8/renault_small_T2 -c experiments/exp8/contraintes.xml | tee experiments/exp8/drc_trim3_small_result_cumul_const)
+(cd ../.. ; ./run.sh Recom2 jointree experiments/exp8/renault_small_T3 10 -s 10 -e -rb experiments/exp8/ -x 2 experiments/exp8/renault_small_T1/ experiments/exp8/renault_small_T2 -c experiments/exp8/contraintes.xml | tee experiments/exp8/jointree_trim3_small_result_cumul_const)
+(cd ../.. ; ./run.sh Recom2 oracle experiments/exp8/renault_small_T3 10 -s 10 -e -rb experiments/exp8/ -x 2 experiments/exp8/renault_small_T1/ experiments/exp8/renault_small_T2 -c experiments/exp8/contraintes.xml | tee experiments/exp8/oracle_trim3_small_result_cumul_const)
+
+echo Trimester 4
+(cd ../.. ; ./run.sh Recom2 drc experiments/exp8/renault_small_T4 10 -s 10 -e -rb experiments/exp8/ -x 3 experiments/exp8/renault_small_T1/ experiments/exp8/renault_small_T2 experiments/exp8/renault_small_T3 -c experiments/exp8/contraintes.xml | tee experiments/exp8/drc_trim4_small_result_cumul_const)
+(cd ../.. ; ./run.sh Recom2 jointree experiments/exp8/renault_small_T4 10 -s 10 -e -rb experiments/exp8/ -x 3 experiments/exp8/renault_small_T1/ experiments/exp8/renault_small_T2 experiments/exp8/renault_small_T3 -c experiments/exp8/contraintes.xml | tee experiments/exp8/jointree_trim4_small_result_cumul_const)
+(cd ../.. ; ./run.sh Recom2 oracle experiments/exp8/renault_small_T4 10 -s 10 -e -rb experiments/exp8/ -x 3 experiments/exp8/renault_small_T1/ experiments/exp8/renault_small_T2 experiments/exp8/renault_small_T3 -c experiments/exp8/contraintes.xml | tee experiments/exp8/oracle_trim4_small_result_cumul_const)
+
