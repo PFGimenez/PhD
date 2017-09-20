d926bc3ee09fcd2628a979e7b295f4b446ffb612
PF <pierre-francois.gimenez@irit.fr>
2017-09-20 16:55:24 +0200

    R script to learn BN network from Java

---
+++ script_learn_BN.R
@@ -0,0 +1,96 @@
+#!/usr/bin/Rscript
+
+{
+  # Parameters : outfile header dataset1 dataset2 …
+  
+  args = commandArgs(trailingOnly=TRUE)
+  if(length(args) < 3)
+  {
+    stop("Pas assez de paramètres ! Paramètres : outfile header dataset1 dataset2 ...")
+  }
+  fichier = args[1]
+  header = eval(parse(text=args[2])) # on convertit le texte en booléen
+  dataset = args[3:length(args)]
+  library(bnlearn, lib.loc="~/R_libs")
+
+  for(j in 1:length(dataset))
+  {
+    print(dataset[j])
+    if(j == 1)
+    {
+      training_set = read.csv(file=dataset[j], header=header)
+    }
+    else
+    {
+      training_set = rbind(training_set, read.csv(file=dataset[j], header=header))
+    }
+  }
+  
+  training_set[] = lapply(training_set, as.character)
+  
+  for(k in 1:dim(training_set)[2])
+  {
+    if(length(c(unique(training_set[,k]))) == 1) # ajout d'une valeur artificielle si besoin est
+    {
+      training_set[,k] = factor(training_set[,k],levels=c(999,unique(training_set[,k])))
+    }
+    else
+    {
+      training_set[,k] = factor(training_set[,k],levels=c(unique(training_set[,k])))
+    }
+  }
+  
+  bn = hc(training_set) #apprentissage de la structure
+  fitted = bn.fit(bn, training_set, iss = 1, method = "bayes")
+
+    cat("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n", file=fichier)
+    
+    cat("<!-- DTD for the XMLBIF 0.3 format -->\n<!DOCTYPE BIF [\n<!ELEMENT BIF ( NETWORK )*>\n<!ATTLIST BIF VERSION CDATA #REQUIRED>\n   <!ELEMENT NETWORK ( NAME, ( PROPERTY | VARIABLE | DEFINITION )* )>\n      <!ELEMENT NAME (#PCDATA)>\n        <!ELEMENT VARIABLE ( NAME, ( OUTCOME |  PROPERTY )* ) >\n          <!ATTLIST VARIABLE TYPE (nature|decision|utility) \"nature\">\n           <!ELEMENT OUTCOME (#PCDATA)>\n            <!ELEMENT DEFINITION ( FOR | GIVEN | TABLE | PROPERTY )* >\n              <!ELEMENT FOR (#PCDATA)>\n                <!ELEMENT GIVEN (#PCDATA)>\n                  <!ELEMENT TABLE (#PCDATA)>\n                    <!ELEMENT PROPERTY (#PCDATA)>\n                      ]>\n", file=fichier, append=TRUE)
+    
+    cat("<BIF VERSION=\"0.3\">\n",file=fichier, append=TRUE)
+    cat("<NETWORK>\n",file=fichier, append=TRUE)
+    
+    cat("<NAME>Reco</NAME>\n",file=fichier, append=TRUE)
+    # Variables
+    for(v in 1:dim(training_set)[2])
+    {
+      cat("<VARIABLE TYPE=\"nature\">\n", file=fichier, append=TRUE)
+      cat(paste(paste("\t<NAME>",colnames(training_set)[v],sep=""),"</NAME>\n",sep=""), file=fichier, append=TRUE)
+      #      cat("\t<TYPE>discrete</TYPE>\n", file=fichier, append=TRUE)
+      if(is.element("999",levels(training_set[,v])))
+      {
+        cat("\t<OUTCOME>999</OUTCOME>\n", file=fichier, append=TRUE)
+      }
+      for(w in 1:length(levels(factor(training_set[,v]))))
+      {
+        cat(paste(paste("\t<OUTCOME>",levels(factor(training_set[,v]))[w],sep=""),"</OUTCOME>\n",sep=""), file=fichier, append=TRUE)
+      }
+      cat("</VARIABLE>\n\n", file=fichier, append=TRUE)
+    }
+    
+    # Probabilités
+    for(v in 1:dim(training_set)[2])
+    {
+      cat("<DEFINITION>\n", file=fichier, append=TRUE)
+      cat(paste(paste("\t<FOR>",fitted[[v]]$node,sep=""),"</FOR>\n",sep=""), file=fichier, append=TRUE)
+      if(length(fitted[[v]]$parents) > 0)
+      {
+        for(w in 1:length(fitted[[v]]$parents))
+        {
+          cat(paste(paste("\t<GIVEN>",fitted[[v]]$parents[[length(fitted[[v]]$parents)-w+1]],sep=""),"</GIVEN>\n",sep=""), file=fichier, append=TRUE)
+        }
+      }
+      cat("\t<TABLE>", file=fichier, append=TRUE)
+      
+      for(w in 1:(length(fitted[[v]]$prob)))
+      {
+        cat(paste(fitted[[v]]$prob[[w]],""), file=fichier, append=TRUE)
+      }
+      
+      cat("</TABLE>\n", file=fichier, append=TRUE)
+      cat("</DEFINITION>\n\n", file=fichier, append=TRUE)
+      
+    }
+    cat("</NETWORK>\n",file=fichier, append=TRUE)
+    cat("</BIF>\n",file=fichier, append=TRUE)
+}
